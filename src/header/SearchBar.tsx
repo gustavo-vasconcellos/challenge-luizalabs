fc8aced63464bb2c93be5ee1a9455156ed6a4681
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2021-06-27 22:12:27 -0300

    Fix prettier into SearchBar

--- src/header/SearchBar.tsx
+++ src/header/SearchBar.tsx
@@ -4,7 +4,10 @@ import { FaSearch } from 'react-icons/fa'
 const SearchBar: React.FunctionComponent = () => {
   const [text, setText] = useState('')
   return (
-    <div className="flex pa2 ph3 bg-white items-center br2 mt2" data-test-id="search-bar">
+    <div
+      className="flex pa2 ph3 bg-white items-center br2 mt2"
+      data-test-id="search-bar"
+    >
       <span className="mr3 gray db">
         <FaSearch />
       </span>
