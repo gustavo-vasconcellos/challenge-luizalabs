bd0054fb549b6c7838ba93ea87cd8cb48e7ee2cb
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2021-06-28 20:46:11 -0300

    add product name test

---
+++ src/__tests__/ProducName.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import ProductName from '../product-summary/ProductName'
+
+describe('ProductName', () => {
+  it('renders', () => {
+    render(<ProductName name="Test" />)
+
+    expect(screen.getByTestId('product-name')).toBeInTheDocument()
+  })
+
+  it('not contain the text prop "name" passed in the element', () => {
+    render(<ProductName name="Wrong test" />)
+
+    expect(screen.getByTestId('product-name')).not.toHaveTextContent('Test')
+  })
+
+  it('contain the text prop "name" passed in the element', () => {
+    render(<ProductName name="Test" />)
+
+    expect(screen.getByTestId('product-name')).toHaveTextContent('Test')
+  })
+})
