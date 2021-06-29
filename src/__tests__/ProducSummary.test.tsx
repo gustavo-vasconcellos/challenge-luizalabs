71a85fbefbb92c194a32da9e6eff15a945bc5f17
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2021-06-28 21:58:36 -0300

    Add product summary test

---
+++ src/__tests__/ProducSummary.test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import React from 'react'
+import ProductSummary from '../product-summary/ProductSummary'
+import { mockProduct, renderWithRouter } from '../test-utils'
+
+describe('ProductSummary', () => {
+  it('renders', () => {
+    renderWithRouter(<ProductSummary product={mockProduct} />)
+
+    expect(screen.getByTestId('product-name')).toBeInTheDocument()
+  })
+
+  it('contain each inner components', () => {
+    renderWithRouter(<ProductSummary product={mockProduct} />)
+
+    expect(screen.getByTestId('product-name')).toHaveTextContent(
+      'Camisa Nike Corinthians I'
+    )
+    expect(screen.getByTestId('product-price')).toHaveTextContent('R$ 229.90')
+    expect(screen.getByTestId('product-image')).toHaveProperty(
+      'src',
+      'https://via.placeholder.com/300x300'
+    )
+  })
+})
