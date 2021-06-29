e8ec1bc381aab02d5edecf9b1866c4ee98b66c74
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2021-06-28 21:47:40 -0300

    Add product price test

---
+++ src/__tests__/ProducPrice.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import ProductPrice from '../product-summary/ProductPrice'
+import { mockProduct } from '../test-utils'
+
+describe('ProductPrice', () => {
+  it('renders', () => {
+    render(<ProductPrice price={1} currencyFormat="$" />)
+
+    expect(screen.getByTestId('product-price')).toBeInTheDocument()
+  })
+
+  it('contain USD as currency value', () => {
+    render(<ProductPrice price={1} currencyFormat="$" />)
+
+    expect(screen.getByTestId('product-price')).toHaveTextContent('$ 1.00')
+  })
+
+  it('contain BRL as currency value', () => {
+    render(<ProductPrice price={1} currencyFormat="R$" />)
+
+    expect(screen.getByTestId('product-price')).toHaveTextContent('R$ 1.00')
+  })
+
+  it('contain correct mocked value', () => {
+    render(<ProductPrice price={mockProduct.price} currencyFormat="R$" />)
+
+    expect(screen.getByTestId('product-price')).toHaveTextContent('R$ 229.90')
+  })
+})
